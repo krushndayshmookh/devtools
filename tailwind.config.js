3dcd27d46179e2da6728dcdc6a0e766f96ac7d06
Krushn Dayshmookh <krushn.dayshmookh.ext@siemens.com>
2023-09-14 22:49:50 +0530

    install daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html', 
+    './src/**/*.{vue,js,ts}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [require('daisyui')]
 }
-
