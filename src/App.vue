dd277576bec4006c52fbef295966ebe7e3c0b1eb
Krushn Dayshmookh <krushn.dayshmookh.ext@siemens.com>
2023-09-15 10:47:44 +0530

    create about page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <header>
     <div class="navbar bg-base-200">
       <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">Krushn's Devtools</a>
+        <router-link to="/" class="btn btn-ghost normal-case text-xl">
+          Krushn's Devtools
+        </router-link>
       </div>
       <div class="flex-none">
         <ul class="menu menu-horizontal px-1">
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container mx-auto p-8 prose">
+    <h1 class="text-center text-3xl font-bold">About</h1>
+
+    <p class="text-center text-lg mt-4">
+      This is a collection of tools that can be used to help with your
+      development workflow.
+    </p>
+
+    <p class="text-center text-lg mt-4">
+      This is a work in progress. More tools to come!
+    </p>
+
+    <h2 class="text-center text-2xl font-bold mt-8">Contribute</h2>
+
+    <p class="text-center text-lg mt-4">
+      This project is open source. Contributions are welcome!
+
+      <br />
+      <a
+        href="https://github.com/krushndayshmookh/devtools"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-blue-500 hover:underline"
+      >
+        View on GitHub
+      </a>
+    </p>
+
+    <h2 class="text-center text-2xl font-bold mt-8">About Me</h2>
+
+    <p class="text-center text-lg mt-4">
+      My name is Krushn Dayshmookh. I am a software engineer based in Pune,
+      India. I am passionate about building software that is scalable,
+      maintainable, and user-friendly.
+
+      <br />
+
+      <a
+        href="https://dayshmookh.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-blue-500 hover:underline"
+      >
+        Visit my website
+      </a>
+    </p>
   </div>
 </template>
