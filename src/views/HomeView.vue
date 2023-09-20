f22b9de6d7b0cc2a5f17711a98e25247095bd2a2
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2023-09-20 12:13:59 +0530

    Add support for external links on HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="home container mx-auto p-8">
     <div class="grid grid-cols-3 gap-8">
-      <router-link v-for="app in apps" :key="app.link" :to="`/${app.link}`">
-        <div
-          class="card card-side bg-base-200 hover:bg-base-300 shadow-sm mb-8"
-        >
-          <figure v-if="app.imageURL">
-            <img :src="app.imageURL" alt="Movie" />
-          </figure>
-          <div class="card-body">
-            <h2 class="card-title">{{ app.label }}</h2>
-            <p>{{ app.description }}</p>
+      <div v-for="app in apps" :key="app.label">
+        <router-link v-if="app.link" :to="`/${app.link}`">
+          <div
+            class="card card-side bg-base-200 hover:bg-base-300 shadow-sm mb-8"
+          >
+            <figure v-if="app.imageURL">
+              <img :src="app.imageURL" alt="Movie" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">{{ app.label }}</h2>
+              <p>{{ app.description }}</p>
+            </div>
           </div>
-        </div>
-      </router-link>
+        </router-link>
+  
+        <a v-if="app.externalURL" :to="app.externalURL" target="_blank">
+          <div
+            class="card card-side bg-base-200 hover:bg-base-300 shadow-sm mb-8"
+          >
+            <figure v-if="app.imageURL">
+              <img :src="app.imageURL" alt="Movie" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">{{ app.label }}</h2>
+              <p>{{ app.description }}</p>
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -24,8 +40,16 @@ const apps = [
     link: 'gradients',
     label: 'Gradients',
     description: 'A collection of gradients that can be used in applications.',
-    imageURL: ''
+    imageURL: '',
+    externalURL: ''
     // imageURL: 'https://picsum.photos/160/210'
+  },
+  {
+    link: '',
+    label: 'SVG Icons',
+    description: 'A collection of SVG Icons.',
+    imageURL: '',
+    externalURL: 'http://svgicons.sparkk.fr/'
   }
   // {
   //   link: 'waves',
